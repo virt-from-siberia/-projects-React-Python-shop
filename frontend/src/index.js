9429e0dcb53bdabfa8ed1124a5ffa9029309d937
Aleksey <Elchin>
2022-04-03 00:50:46 +0900

    python react notebook

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
+import App from "./App";
 import "./index.css";
 import "./bootstrap.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,26 +1,40 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import { Product } from "../components/Product";
+import React, {useState, useEffect} from "react";
+import {Row, Col} from "react-bootstrap";
+import axios from 'axios'
 
-import products from "../products";
+import {Product} from "../components/Product";
+
 
 export function HomeScreen() {
-  return (
-    <div>
-      <h1>Latest products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col
-            sm={12}
-            md={6}
-            lg={4}
-            xl={3}
-            key={product._id}
-          >
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        async function fetchProducts() {
+            const {data} = await axios.get('/api/products/').then()
+            console.log('data', data)
+            setProducts(data)
+        }
+        fetchProducts()
+
+    }, [])
+
+
+    return (
+        <div>
+            <h1>Latest products</h1>
+            <Row>
+                {products.length > 0 && products.map((product) => (
+                    <Col
+                        sm={12}
+                        md={6}
+                        lg={4}
+                        xl={3}
+                        key={product._id}
+                    >
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
 }
