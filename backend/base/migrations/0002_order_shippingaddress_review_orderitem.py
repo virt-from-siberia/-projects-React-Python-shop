ee8012a87d9e701207fadfd6f0d364d5c3541b1d
Aleksey <Elchin>
2022-04-21 06:37:48 +0900

    python react eshop notebook

--- backend/base/migrations/0002_order_shippingaddress_review_orderitem.py
+++ backend/base/migrations/0002_order_shippingaddress_review_orderitem.py
@@ -16,17 +16,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Order',
             fields=[
-                ('paymentMethod', models.CharField(blank=True, max_length=255, null=True)),
-                ('taxPrice', models.DecimalField(blank=True, decimal_places=2, max_digits=7, null=True)),
-                ('shippingPrice', models.DecimalField(blank=True, decimal_places=2, max_digits=7, null=True)),
-                ('totalPrice', models.DecimalField(blank=True, decimal_places=2, max_digits=7, null=True)),
+                ('paymentMethod', models.CharField(
+                    blank=True, max_length=255, null=True)),
+                ('taxPrice', models.DecimalField(blank=True,
+                 decimal_places=2, max_digits=7, null=True)),
+                ('shippingPrice', models.DecimalField(
+                    blank=True, decimal_places=2, max_digits=7, null=True)),
+                ('totalPrice', models.DecimalField(blank=True,
+                 decimal_places=2, max_digits=7, null=True)),
                 ('isPaid', models.BooleanField(default=False)),
                 ('paidAt', models.DateTimeField(blank=True, null=True)),
                 ('isDelivered', models.BooleanField(default=False)),
                 ('deliveredAt', models.DateTimeField(blank=True, null=True)),
                 ('createdAt', models.DateTimeField(auto_now_add=True)),
-                ('_id', models.AutoField(editable=False, primary_key=True, serialize=False)),
-                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+                ('_id', models.AutoField(editable=False,
+                 primary_key=True, serialize=False)),
+                ('user', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -34,11 +40,15 @@ class Migration(migrations.Migration):
             fields=[
                 ('address', models.CharField(blank=True, max_length=255, null=True)),
                 ('city', models.CharField(blank=True, max_length=255, null=True)),
-                ('postalCode', models.CharField(blank=True, max_length=255, null=True)),
+                ('postalCode', models.CharField(
+                    blank=True, max_length=255, null=True)),
                 ('country', models.CharField(blank=True, max_length=255, null=True)),
-                ('shippingPrice', models.DecimalField(blank=True, decimal_places=2, max_digits=7, null=True)),
-                ('_id', models.AutoField(editable=False, primary_key=True, serialize=False)),
-                ('order', models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='base.order')),
+                ('shippingPrice', models.DecimalField(
+                    blank=True, decimal_places=2, max_digits=7, null=True)),
+                ('_id', models.AutoField(editable=False,
+                 primary_key=True, serialize=False)),
+                ('order', models.OneToOneField(blank=True, null=True,
+                 on_delete=django.db.models.deletion.CASCADE, to='base.order')),
             ],
         ),
         migrations.CreateModel(
@@ -47,9 +57,12 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(blank=True, max_length=255, null=True)),
                 ('rating', models.IntegerField(blank=True, default=0, null=True)),
                 ('comment', models.TextField(blank=True, null=True)),
-                ('_id', models.AutoField(editable=False, primary_key=True, serialize=False)),
-                ('product', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.product')),
-                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+                ('_id', models.AutoField(editable=False,
+                 primary_key=True, serialize=False)),
+                ('product', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.product')),
+                ('user', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -57,11 +70,15 @@ class Migration(migrations.Migration):
             fields=[
                 ('name', models.CharField(blank=True, max_length=255, null=True)),
                 ('qty', models.IntegerField(blank=True, default=0, null=True)),
-                ('price', models.DecimalField(blank=True, decimal_places=2, max_digits=7, null=True)),
+                ('price', models.DecimalField(blank=True,
+                 decimal_places=2, max_digits=7, null=True)),
                 ('image', models.CharField(blank=True, max_length=255, null=True)),
-                ('_id', models.AutoField(editable=False, primary_key=True, serialize=False)),
-                ('order', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.order')),
-                ('product', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.product')),
+                ('_id', models.AutoField(editable=False,
+                 primary_key=True, serialize=False)),
+                ('order', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.order')),
+                ('product', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.product')),
             ],
         ),
     ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,22 +1,70 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 from .models import Product
 from .products import products
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exist'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [{"lol": "lol"}]
-    return Response("lol")
+@permission_classes([IsAuthenticated])
+def getUsrProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
